f69ec19180e47e2c0ac37a5edb6086354b47eb10
ConnorC04 <connorjcasey@iCloud.com>
2025-04-09 16:09:19 -0400

    ding fries are done

--- src/main/java/com/zipcodeconway/ConwayGameOfLife.java
+++ src/main/java/com/zipcodeconway/ConwayGameOfLife.java
@@ -9,24 +9,36 @@ public class ConwayGameOfLife {
     }
 
     public static void main(String[] args) {
-        ConwayGameOfLife sim = new ConwayGameOfLife(50);
-        int[][] endingWorld = sim.simulate(50);
+        ConwayGameOfLife conwayGameOfLife = new ConwayGameOfLife(5);
+        int[][] conway = conwayGameOfLife.createRandomStart(5);
+        ConwayGameOfLife sim = new ConwayGameOfLife(5, conway);
     }
 
     // Contains the logic for the starting scenario.
     // Which cells are alive or dead in generation 0.
     // allocates and returns the starting matrix of size 'dimension'
     private int[][] createRandomStart(Integer dimension) {
-        return new int[1][1];
+        return new int[dimension][dimension];
     }
 
     public int[][] simulate(Integer maxGenerations) {
+
+        while (maxGenerations > 0){
+
+            maxGenerations--;
+        }
         return new int[1][1];
     }
 
     // copy the values of 'next' matrix to 'current' matrix,
     // and then zero out the contents of 'next' matrix
     public void copyAndZeroOut(int [][] next, int[][] current) {
+        for (int i = 0; i < current.length; i++){
+            for (int j = 0; j < current.length; j++){
+                current[i][j] = next[i][j];
+                next[i][j] = 0;
+            }
+        }
     }
 
     // Calculate if an individual cell should be alive in the next generation.
@@ -38,6 +50,9 @@ public class ConwayGameOfLife {
 		Any dead cell with exactly three live neighbours cells will come to life.
 	*/
     private int isAlive(int row, int col, int[][] world) {
-        return 0;
+        if (world[row][col] == 1) {
+            return 0;
+        }
+        return 1;
     }
 }
